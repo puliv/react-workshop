b336e97b5568dadf7efd15f89616bdc626e74bbb
storo <sergio.toro@mercadolibre.cl>
2019-08-01 13:54:44 -0400

    event's base

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.scss';
 
+// ## Actividad
+// Agregar una etiqueta donde se muestre un el valor de un contador inicializado en 0.
+// Agregar boton que incremente en uno el contador.
+// Agregar boton que reste uno al contador;
+// Agregar boton que haga reset del contador volviendo a su valor inicial.
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, World! </h1>
+        <button type="button">click!</button>
       </div>
     );
   }
