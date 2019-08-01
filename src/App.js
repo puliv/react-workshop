f669ea55c999540d30551ac7dc1b1b30a53eab4b
Luis Fuenmayor <fuelusumar@gmail.com>
2019-08-01 17:51:01 -0400

    dataflow examples

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import './App.scss';
 import Avatar from './components/Avatar';
 import Greeting from './components/Greeting';
 
-const user = {
-  firstName: 'Luis',
-  lastName: 'Fuenmayor',
-  avatar: 'http://olegif.com/bin/gifs/00/25/06.gif',
-};
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: 'Luis',
+      lastName: 'Fuenmayor',
+      avatar: 'http://olegif.com/bin/gifs/00/25/06.gif',
+    };
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
+
+  handleOnClick() {
+    this.setState({ firstName: 'Sergio Chayanne', lastName: 'Toro' });
+  }
+
   render() {
     return (
       <div className="App">
-        <Greeting firstName={user.firstName} lastName={user.lastName} />
-        <Avatar picture={user.avatar} />
+        <Greeting firstName={this.state.firstName} lastName={this.state.lastName} />
+        <Avatar picture={this.state.avatar} />
+        <button type="button" onClick={this.handleOnClick}>
+          Cambiar nombre
+        </button>
       </div>
     );
   }
